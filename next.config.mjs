a926a23959f9c2b42175fd469c9954aa9c89a5a9
Shifu <aemre3113@gmail.com>
2024-06-03 12:48:44 +0300

    next.config ve jsconfig eklemeler yapıldı.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ["en", "tr"],
+    defaultLocale: "tr",
+    localeDetection: false,
+  },
+  trailingSlash: true,
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
