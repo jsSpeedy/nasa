87429d03d5080d33d151941f5002e40b449d529b
Shifu <aemre3113@gmail.com>
2024-06-14 09:36:51 +0300

    container yapısı oluşturuldu.

---
+++ src/app/components/atoms/Container.js
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { device } from "src/utils/device";
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ContainerWrapper = styled.div`
+  width: 100%;
+  max-width: 1440px; /* veya istediğiniz maksimum genişlik */
+  margin: 0 auto;
+  padding: 0 20px; /* Örnek bir padding değeri */
+
+  @media ${device.laptopL} {
+    padding: 0 15px; /* Laptop büyük ekranlar için ayar */
+  }
+
+  @media ${device.laptop} {
+    padding: 0 10px; /* Laptop orta boy ekranlar için ayar */
+  }
+
+  @media ${device.tablet} {
+    padding: 0 5px; /* Tabletler için ayar */
+  }
+
+  @media ${device.mobileL} {
+    padding: 0 3px; /* Büyük telefonlar için ayar */
+  }
+
+  @media ${device.mobileM} {
+    padding: 0 2px; /* Orta büyüklükteki telefonlar için ayar */
+  }
+
+  @media ${device.mobileS} {
+    padding: 0 1px; /* Küçük telefonlar için ayar */
+  }
+`;
+
+const Container = ({ children }) => {
+  return (
+    <ContainerWrapper>
+      <Row>{children}</Row>
+    </ContainerWrapper>
+  );
+};
+
+export default Container;
