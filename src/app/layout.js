f026f9562c806d789dd2c894810c99a710523458
Shifu <aemre3113@gmail.com>
2024-06-14 15:46:25 +0300

    font yapısı oluşturuldu.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import GlobalStyle from "./components/atoms/GlobalStyles";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "NASA",
@@ -11,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <GlobalStyle />
         {children}
       </body>
