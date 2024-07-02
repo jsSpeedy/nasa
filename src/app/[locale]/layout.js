7001c7348938a41bae3eb96abd2b705e4a3e26b5
Shifu <aemre3113@gmail.com>
2024-07-02 15:57:39 +0300

    font değiştirildi.

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,6 +1,9 @@
 import GlobalStyle from "../../components/atoms/GlobalStyles";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { Exo_2 } from "next/font/google";
+
+const exo_2 = Exo_2({ subsets: ["latin"] });
 
 export const metadata = {
   title: "NASA",
@@ -11,7 +14,7 @@ export default async function RootLayout({ children, params: { locale } }) {
   const messages = await getMessages();
   return (
     <html lang={locale}>
-      <body>
+      <body className={exo_2.className}>
         <GlobalStyle />
         <NextIntlClientProvider messages={messages}>
           {children}
--- src/components/atoms/GlobalStyles.js
+++ src/components/atoms/GlobalStyles.js
@@ -23,7 +23,6 @@ body {
 
 body {
   background: var(--clr);
-  font-family: 'Nexa',sans-serif;
   font-weight: 700;
   font-size: 18px;
 }
@@ -35,13 +34,6 @@ a {
 
 main {
   padding-top: 100px;
-}
-
-@font-face {
-  font-family: 'Nexa';
-  src: url('/fonts/Nexa/NexaTextDemo-Light.woff2') format('woff2'),
-       url('/fonts/Nexa/NexaTextDemo-Light.woff') format('woff');
-       font-display: fallback;
 }
 
 `;
