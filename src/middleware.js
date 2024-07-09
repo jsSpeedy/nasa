8a351c19c7086fdad06edba1a99172defc51f2ae
Shifu <aemre3113@gmail.com>
2024-07-09 09:21:06 +0300

    url tekrarlama ve sayfa yenileme sorunu düzeltildi.

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,12 @@
 import createMiddleware from "next-intl/middleware";
+import { pathnames } from "./config";
 
 export default createMiddleware({
   locales: ["en", "tr"],
   defaultLocale: "tr",
+  pathnames,
 });
 
 export const config = {
-  matcher: ["/", "/(tr|en)/:path*"],
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
